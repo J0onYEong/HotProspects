d7ba92d8e38b2bab31ec5db99c36f51bdd55f77a
choijun0 <cjy00050005@gmail.com>
2023-01-29 16:30:21 +0900

    Initial Commit

---
+++ HotProspects/HotProspectsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HotProspectsApp.swift
+//  HotProspects
+//
+//  Created by 최준영 on 2023/01/29.
+//
+
+import SwiftUI
+
+@main
+struct HotProspectsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
